e3d62ba04708033dffa2ee63d2f7cf82a695e74c
George Papidas <george.papidas@gmail.com>
2023-07-22 20:33:50 +0300

    Added cache in area_autocomplete_options

--- app/controllers/property_ad_locations_controller.rb
+++ app/controllers/property_ad_locations_controller.rb
@@ -2,8 +2,15 @@ class PropertyAdLocationsController < ApplicationController
   def area_autocomplete_options
     input = params[:input]
 
-    response = HTTParty.get("https://4ulq3vb3dogn4fatjw3uq7kqby0dweob.lambda-url.eu-central-1.on.aws/?input=#{input}")
+    cached_response = Rails.cache.read("area_autocomplete/#{input}")
+    if cached_response
+      render json: cached_response
+    else
+      response = HTTParty.get("https://4ulq3vb3dogn4fatjw3uq7kqby0dweob.lambda-url.eu-central-1.on.aws/?input=#{input}")
 
-    render json: response.parsed_response
+      Rails.cache.write("area_autocomplete/#{input}", response.parsed_response, expires_in: 1.day)
+
+      render json: response.parsed_response
+    end
   end
 end
