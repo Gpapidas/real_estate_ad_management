d841670f308f35cb7518b6af1aab6b3a22c106ea
George Papidas <george.papidas@gmail.com>
2023-07-23 15:12:30 +0300

    Encoding input on endpoint for correct external API call

--- app/controllers/property_ad_locations_controller.rb
+++ app/controllers/property_ad_locations_controller.rb
@@ -1,12 +1,13 @@
 class PropertyAdLocationsController < ApplicationController
   def area_autocomplete_options
     input = params[:input]
+    encoded_input = URI.encode_www_form_component(input)
     begin
       cached_response = Rails.cache.read("area_autocomplete/#{input}")
       if cached_response
         render json: cached_response
       else
-        response = HTTParty.get("https://4ulq3vb3dogn4fatjw3uq7kqby0dweob.lambda-url.eu-central-1.on.aws/?input=#{input}")
+        response = HTTParty.get("https://4ulq3vb3dogn4fatjw3uq7kqby0dweob.lambda-url.eu-central-1.on.aws/?input=#{encoded_input}")
 
         Rails.cache.write("area_autocomplete/#{input}", response.parsed_response, expires_in: 1.day)
 
